db49ba146107a10825d491338271b9ea0581e5cd
Mediagenic <veniamin@belyaev.work>
2025-08-21 15:06:29 +0300

    Minor changes

--- src/main/java/xyz/mediagenic/tmdb_cli/TmdbCliApplication.java
+++ src/main/java/xyz/mediagenic/tmdb_cli/TmdbCliApplication.java
@@ -1,13 +1,7 @@
 package xyz.mediagenic.tmdb_cli;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import xyz.mediagenic.tmdb_cli.TMDBInteraction.QueryType;
-import xyz.mediagenic.tmdb_cli.TMDBInteraction.TMDBQuery;
-
-import java.io.IOException;
-import java.net.URISyntaxException;
 
 @SpringBootApplication
 public class TmdbCliApplication {
